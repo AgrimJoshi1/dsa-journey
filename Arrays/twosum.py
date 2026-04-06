01c5557a9575aee03382e13a9a1cd8918c4627f5
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-04-06 22:12:10 +0530

    two sum

---
+++ Arrays/twosum.py
@@ -0,0 +1,15 @@
+#QUES NO - 1
+'''TWO SUM'''
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        d = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in d:
+                return [d[target - nums[i]], i]
+            d[nums[i]] = i
+
+# Approach:
+# - Use a dictionary to store value -> index
+# - For each element, check if (target - current element) already exists (key value pair)
+# - If yes, we found the pair
