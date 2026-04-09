f7b21864d9dfa8fae78d8d0e151e57747a28d082
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-04-09 22:32:54 +0530

    No. 912

---
+++ Arrays/sort.py
@@ -0,0 +1,7 @@
+#Problem No. 912 - Sort an array
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        return sorted(nums)
+        
+#Approach 
+#Used inbuilt sort function 
