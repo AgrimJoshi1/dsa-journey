49c1147a5ad5a82002ba37742e4f5bc3ab092010
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-05-14 20:47:50 +0530

    binary add

---
+++ Math/binadd.py
@@ -0,0 +1,10 @@
+#Problem No. 67 - Add Binary
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        
+        num1 = int(a, 2)
+        num2 = int(b, 2)
+
+        total = num1 + num2
+        return bin(total)[2:]
+        
