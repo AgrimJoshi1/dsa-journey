636f2bf67f826c1d9d016c56317d483ef0b923f6
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-06-13 21:11:11 +0530

    ques

---
+++ Arrays/longestpar.py
@@ -0,0 +1,16 @@
+#Problem No. 32 - Longest Valid Parenthesis
+class Solution(object):
+    def longestValidParentheses(self, s):
+        stack = [-1]
+        count = 0
+        
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    count = max(count, i - stack[-1])
+        return count
