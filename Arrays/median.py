2e5ffba6cc48a3e5a62ea092eac7a0a11cbe1f3d
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-06-22 23:03:28 +0530

    change

--- Arrays/median.py
+++ Arrays/median.py
@@ -9,4 +9,4 @@ class Solution:
         if n % 2 == 1:
             return arr[n // 2]
         return (arr[n // 2] + arr[n // 2 - 1]) / 2
-# new method to take median if array is sorted
+# new method to take median if array is sorted.
