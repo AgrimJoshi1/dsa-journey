30fef56f6fb568cd621004891103f14459023abb
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-07-10 23:36:54 +0530

    ques

---
+++ Arrays/missingnum.java
@@ -0,0 +1,19 @@
+//Problem No. 268 - Missing NUmber
+package Arrays;
+
+import java.util.Arrays;
+
+public class missingnum {
+    class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int x=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==x){
+                x++;
+            }
+        }
+        return x;
+    }
+} 
+}
