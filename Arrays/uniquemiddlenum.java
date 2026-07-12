f9fa251c2eae63d1d7844dea93b26283ae697d1d
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-07-12 22:12:21 +0530

    ques

---
+++ Arrays/uniquemiddlenum.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class uniquemiddlenum {
+    class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        int m = nums[nums.length / 2];
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == m) {
+                count++;
+            }
+        }
+
+        return count == 1;
+    }
+}
+}
