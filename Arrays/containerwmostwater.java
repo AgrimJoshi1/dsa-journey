b6f50cf3bc25013964312cd45502e3abf6a115a2
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-07-25 23:09:49 +0530

    ques

---
+++ Arrays/containerwmostwater.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class containerwmostwater {
+    class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+
+        for (int i = 0; i < height.length - 1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int width = j - i;
+                int h = Math.min(height[i], height[j]);
+                int area = width * h;
+
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
+    
+}
