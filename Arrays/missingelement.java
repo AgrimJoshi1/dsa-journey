616dfc58daaf77ce0b306e95069b5bf9c9d7c0a2
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-08-04 21:52:05 +0530

    ques

---
+++ Arrays/missingelement.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class missingelement {
+    class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int x = nums[i - 1] + 1; x < nums[i]; x++) {
+                ans.add(x);
+            }
+        }
+
+        return ans;
+    }
+}
+    
+}
